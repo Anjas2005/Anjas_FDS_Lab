6139acd9def4666a92c0068b0a8ac24aa977abdb
Anjas2005 <118038417+Anjas2005@users.noreply.github.com>
2023-11-03 08:26:21 +0530

    Add files via upload

---
+++ Link_list.cpp
@@ -0,0 +1,129 @@
+public class Linked_List4 {
+  
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public Linked_List4(){
+        this.size = 0;
+    }
+
+    public void insertFirst(int val){
+        Node node = new Node(val);   //Creates a new box.
+        node.next = head;
+        head = node;
+
+        if(tail == null){
+            tail = head;
+        }
+        size += 1;
+    }
+
+    public void insertLast(int val){
+        if(tail == null){
+            insertFirst(val);
+            return;
+        }
+        Node node = new Node(val);
+        tail.next = node;
+        tail = node;
+        size++;
+    }
+
+    public void insert(int val, int index){
+        if(index == 0){
+            insertFirst(val);
+        }
+        if (index == size){
+            insertLast(val);
+        }
+
+        Node temp = head;
+        for(int i = 1; i < index; i++){
+            temp = temp.next;
+        }
+
+        Node node = new Node(val, temp.next);
+            temp.next = node;
+            size++;
+
+    }
+
+
+
+    public int deleteFirst(){
+        int val = head.value;
+        head = head.next;
+        if(head ==  null){
+            tail = null;
+        }
+        size--;
+        return val;
+    }
+
+    public int deleteLast() {
+        if (size <= 1) {
+            return deleteFirst();
+        }
+        Node secondLast = get(size - 2);
+        int val = tail.value;
+        tail = secondLast;
+        tail.next = null;
+        return val;
+    }
+
+    public int delete(int index){
+        if (index == 0){
+            return deleteFirst();
+        }
+        if (index == size-1){
+            return deleteLast();
+        }
+
+        Node prev = get(index -1);
+        int val = prev.next.value;
+        prev.next = prev.next.next;
+        return val;
+    }
+
+    public Node get(int index){
+        Node node = head;
+        for(int i = 0; i < index; i++){
+            node = node.next;
+        }
+        return node;
+    }
+
+    public void display(){
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.value + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("END");
+    }
+
+    private class Node{
+        private int value;
+        private Node next;
+  
+        public Node(int value){
+            this.value = value;
+        }
+  
+        public Node(int value, Node next){
+            this.value = value;
+            this.next = next;
+        }
+  
+  
+    }
+} 
+  
+int main(){
+    Linked_List4 list = Linked_List4();
+    list.insertFirst(5);
+    list.insertFirst(10);
+    list.insertFirst(15);
+    return 0;
+} 
